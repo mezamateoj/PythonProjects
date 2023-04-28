67097036b59011280e5805feeaab94fc10a9c966
Mateo Meza <mezamateoj@gmail.com>
2023-04-28 17:41:04 -0400

    Start day 31

---
+++ 100 Days of Python Udemy/FlashCard/main.py
@@ -0,0 +1,34 @@
+# day 31 Flash card app
+import tkinter as tk
+from tkinter import messagebox
+import random
+
+class FlashCard:
+    def __init__(self, front, back):
+        self.root = tk.Tk()
+        self.root.title('Flash Card')
+        self.root.config(padx=50, pady=50)
+        self.front = front
+        self.back = back
+
+
+    def canvas(self):
+        pass
+
+    def front(self):
+        pass
+
+    def back(self):
+        pass
+
+    def flip(self):
+        pass
+
+    def is_known(self):
+        pass
+
+    def next_card(self):
+        pass
+
+    
+
